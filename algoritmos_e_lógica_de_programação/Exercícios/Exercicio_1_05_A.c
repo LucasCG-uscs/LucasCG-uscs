792666d7e02bf7d1f32e5739ba96dfb8259a8383
Lucas Costa Guimarães <lucas.guimaraes@uscsonline.com.br>
2024-04-03 18:07:54 -0300

    Delete algoritmos_e_lógica_de_programação directory

--- algoritmos_e_lógica_de_programação/Exercícios/Exercicio_1_05_A.c
+++
@@ -1,8 +0,0 @@
-//Exercicio 5: programa que imprime a mensagem "Este é um arquivo em C"
-#include<stdio.h>
-int main(){
-    
-    printf("Este é um programa em C\n");
-    
-    return 0;
-}
--- algoritmos_e_lógica_de_programação/Exercícios/Exercicio_1_05_B.c
+++
@@ -1,8 +0,0 @@
-//Exercicio 5: programa que imprime a mensagem "Este é um arquivo em C"
-#include<stdio.h>
-int main(){
-    
-    printf("Este\né\num\nprograma\nem\nC");
-    
-    return 0;
-}
